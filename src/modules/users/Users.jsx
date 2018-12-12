4ead2c0fc07e30d21c84ad9189258c7dbda8d994
Ava Guerrero <ava.square@gmail.com>
2018-12-13 00:04:16 +0800

    feat: Set-up Users component

---
+++ src/modules/users/Users.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Users extends Component {
+  render() {
+    return (
+      <div className="users">
+      
+	      <h1>Users List</h1>
+
+	      <p>This is the users listing.</p>
+
+      </div>
+    );
+  }
+}
