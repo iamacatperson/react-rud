ac05b312dbb92006ac6d9596ea5c05e47c9a1231
Ava Guerrero <ava.square@gmail.com>
2018-12-12 22:50:28 +0800

    feat: Add set-up files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello world! This is going to be an awesome app!
+      </div>
+    );
+  }
+}
+
+export default App;
