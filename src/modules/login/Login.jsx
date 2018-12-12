91ca4b5abafd0abd2dd272b1aaf6a7f000e9291d
Ava Guerrero <ava.square@gmail.com>
2018-12-13 00:20:50 +0800

    feat: Set-up the Login and Register components

---
+++ src/modules/login/Login.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+
+  render() {
+    return (
+      <div className="users">
+
+	      <h1>Login</h1>
+
+	      <p>This is the login page.</p>
+
+      </div>
+    );
+  }
+}
---
+++ src/modules/register/Register.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Register extends Component {
+
+  render() {
+    return (
+      <div className="users">
+
+	      <h1>Register</h1>
+
+	      <p>This is the register page.</p>
+
+      </div>
+    );
+  }
+}
