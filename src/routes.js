e81168252be51fb1cefd288310d888d1e3a0f31b
Ava Guerrero <ava.square@gmail.com>
2018-12-13 00:21:17 +0800

    feat: Add Login and Register routes

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Users from './modules/users/Users';
+import Login from './modules/login/Login';
+import Register from './modules/register/Register';
+
 
 class Routes extends Component {
   render() {
     return (
       <Switch>  
         <Route exact component={Users} path="/users" />
+        <Route exact component={Login} path="/login" />
+        <Route exact component={Register} path="/register" />
       </Switch>
     );
   }
