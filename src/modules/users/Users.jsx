924c1c90a0ec4e2fb065db7b025c5a61836b88b1
Ava Guerrero <ava.square@gmail.com>
2018-12-13 00:40:39 +0800

    feat: Add basic Table into Users component using Material UI

--- src/modules/users/Users.jsx
+++ src/modules/users/Users.jsx
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 export default class Users extends Component {
-  render() {
-    return (
-      <div className="users">
-      
-	      <h1>Users List</h1>
+	constructor(props) {
+		super(props);
+		this.state = {}
+	}
 
-	      <p>This is the users listing.</p>
+	componentDidMount() {
+		this.getUsers();
+	}
 
-      </div>
-    );
-  }
+	/* fetch users list from API */
+	getUsers() {
+		console.log("GET USERS");
+	}
+
+	render() {
+		return (
+			<div className="users">
+
+				<h1>Users List</h1>
+
+				<Table>
+					<TableHead>
+						<TableRow>
+							<TableCell>ID</TableCell>
+							<TableCell>Avatar</TableCell>
+							<TableCell>First Name</TableCell>
+							<TableCell>Last Name</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						<TableRow>
+							<TableCell>1</TableCell>
+							<TableCell>[Placeholder]</TableCell>
+							<TableCell>Ava</TableCell>
+							<TableCell>Guerrero</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+
+			</div>
+		);
+	}
 }
