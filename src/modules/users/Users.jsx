8f189ed62cc2f97b783e59684ae8d12ad6bb474c
Ava Guerrero <ava.square@gmail.com>
2018-12-14 22:29:45 +0800

    fix: Change table into divs to accommodate table form tag later on

--- src/modules/users/Users.jsx
+++ src/modules/users/Users.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Users extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {}
+		this.state = {};
 	}
 
 	componentDidMount() {
@@ -11,51 +11,31 @@ export default class Users extends Component {
 	}
 
 	/* fetch users list from API */
-	getUsers() {
-		console.log("GET USERS");
-	}
+	getUsers() {}
 
 	render() {
 		return (
 			<div className="users">
-
 				<h1>Users List</h1>
 
-				<table>
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>Avatar</th>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Actions</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>[Avatar]</td>
-							<td>Ava</td>
-							<td>Guerrero</td>
-							<td>Edit Delete</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>[Avatar]</td>
-							<td>Eugene</td>
-							<td>Guerrero</td>
-							<td>Edit Delete</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>[Avatar]</td>
-							<td>Joseph</td>
-							<td>Guerrero</td>
-							<td>Edit Delete</td>
-						</tr>
-					</tbody>
-				</table>
-
+				<div className="users__table">
+					<div>
+						<div>[Photo]</div>
+						<div>First Name</div>
+						<div>Last Name</div>
+						<div>Email</div>
+						<div>Phone</div>
+						<div>Actions</div>
+					</div>
+					<div>
+						<div>()</div>
+						<div>Ava</div>
+						<div>Guerrero</div>
+						<div>ava.guerrero15@gmail.com</div>
+						<div>8888 8888</div>
+						<div>Edit | Delete</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
