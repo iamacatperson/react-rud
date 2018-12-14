fe96be15e05f6707ca925f2c1a976791803e3f15
Ava Guerrero <ava.square@gmail.com>
2018-12-14 23:23:08 +0800

    feat: Create getUsers() method to call users mock API endpoint then render it

--- src/modules/users/Users.jsx
+++ src/modules/users/Users.jsx
@@ -1,21 +1,46 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./Users.scss";
 
 export default class Users extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			users: [],
+			totalUsers: null
+		};
 	}
 
 	componentDidMount() {
 		this.getUsers();
 	}
 
-	/* fetch users list from API */
-	getUsers() {}
+	/**
+	 * gets the user list from the API endpoint
+	 * @param {string} query	search query string
+	 * @param {number} page 	current page
+	 */
+	getUsers(query = "", currentPage = 1) {
+		axios
+			.get(`http://localhost:3001/users?q=${query}`, {
+				params: {
+					_page: currentPage,
+					_limit: 10
+				}
+			})
+			.then(res => {
+				this.setState({
+					users: res.data,
+					totalUsers: res.headers["x-total-count"]
+				});
+			});
+	}
 
 	render() {
+		const { users } = this.state;
+		console.log(this.state.users);
+
 		return (
 			<div className="users">
 				<h1>Users List</h1>
@@ -29,14 +54,18 @@ export default class Users extends Component {
 						<div>Phone</div>
 						<div>Actions</div>
 					</div>
-					<div>
-						<div>()</div>
-						<div>Ava</div>
-						<div>Guerrero</div>
-						<div>ava.guerrero15@gmail.com</div>
-						<div>8888 8888</div>
-						<div>Edit | Delete</div>
-					</div>
+					{users.map(user => {
+						return (
+							<div key={user.id}>
+								<div>{user.avatar}</div>
+								<div>{user.firstName}</div>
+								<div>{user.lastName}</div>
+								<div>{user.email}</div>
+								<div>{user.phone}</div>
+								<div>Edit | Delete</div>
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		);
