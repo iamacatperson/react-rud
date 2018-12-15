7f4e9fa71143aae2759d78c92112a6994381523e
Ava Guerrero <ava.square@gmail.com>
2018-12-15 10:48:15 +0800

    feat: Add onSubmit() method to handle submission of registration form to firebase

--- src/modules/register/Register.jsx
+++ src/modules/register/Register.jsx
@@ -1,14 +1,49 @@
 import React, { Component } from "react";
+import app from "../../base";
 
 export default class Register extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			email: "",
+			password: ""
+		};
 
+		this.onSubmit = this.onSubmit.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
+	}
 
+	onSubmit(e) {
+		e.preventDefault();
+		const { email, password } = this.state;
+
+		try {
+			const user = app
+				.auth()
+				.createUserWithEmailAndPassword(email, password);
+			this.props.history.push("/login");
+		} catch (error) {
+			alert(error);
+		}
+	}
+
+	/**
+	 * handles changing of input fields
+	 * @param {object} e		event object
+	 */
+	handleInputChange(e) {
+		const target = e.target;
+		const value = target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]: value
+		});
 	}
 
 	render() {
+		const { email, password } = this.state;
+
 		return (
 			<div className="users">
 				<h1>Register</h1>
@@ -16,13 +51,21 @@ export default class Register extends Component {
 				<form onSubmit={this.onSubmit}>
 					<label>
 						Email
-						<input name="email" type="email" placeholder="Email" />
+						<input
+							name="email"
+							type="email"
+							onChange={this.handleInputChange}
+							value={email}
+							placeholder="Email"
+						/>
 					</label>
 					<label>
 						Password
 						<input
 							name="password"
 							type="password"
+							onChange={this.handleInputChange}
+							value={password}
 							placeholder="Password"
 						/>
 					</label>
