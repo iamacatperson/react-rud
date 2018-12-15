b45e6c77c7cf5ecccaf42c87347bbde9c655d9c7
Ava Guerrero <ava.square@gmail.com>
2018-12-15 12:12:33 +0800

    refactor: Apply jsPrettier to Routes component

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import app from "./base";
 
-import Users from './modules/users/Users';
-import Login from './modules/login/Login';
-import Register from './modules/register/Register';
-
+import Users from "./modules/users/Users";
+import Login from "./modules/login/Login";
+import Register from "./modules/register/Register";
 
 class Routes extends Component {
   render() {
     return (
-      <Switch>  
+      <Switch>
         <Route exact component={Users} path="/users" />
         <Route exact component={Login} path="/login" />
         <Route exact component={Register} path="/register" />
