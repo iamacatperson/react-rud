10140c3c0dec1e854ed4a8f3b2f30ef27d1ec085
Ava Guerrero <ava.square@gmail.com>
2018-12-15 10:28:44 +0800

    feat: Set-up registration form in Register component

--- src/modules/register/Register.jsx
+++ src/modules/register/Register.jsx
@@ -1,16 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Register extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
 
-  render() {
-    return (
-      <div className="users">
 
-	      <h1>Register</h1>
+	}
 
-	      <p>This is the register page.</p>
+	render() {
+		return (
+			<div className="users">
+				<h1>Register</h1>
 
-      </div>
-    );
-  }
+				<form onSubmit={this.onSubmit}>
+					<label>
+						Email
+						<input name="email" type="email" placeholder="Email" />
+					</label>
+					<label>
+						Password
+						<input
+							name="password"
+							type="password"
+							placeholder="Password"
+						/>
+					</label>
+					<button type="submit">Sign Up</button>
+				</form>
+			</div>
+		);
+	}
 }
